c2766ba7a49a83f66437146eade2812da6518493
lixiang.yang <lxyang6688@163.com>
2019-03-04 00:00:32 +0800

    Code Splitting part1

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        index: './src/index.js',
+        another: './src/another-module.js'
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: "Output Management"
+            title: "Code Splitting"
         })
     ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    name: 'commons',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    minChunks: 2,
+                    enforce: true
+                }
+            }
+        }
+    },
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
